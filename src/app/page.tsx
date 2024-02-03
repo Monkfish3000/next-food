fe7fcf5565f0062457bb1caa1b682a187610f75d
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-02-03 20:23:11 +0100

    styled homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,53 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meals</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className="flex gap-4 m-3 w-9/10 max-w-custom">
+        <div className="w-5 h-5 text-customGray"></div>
+        <div>
+          <div className="hero">
+            <h1 className="font-bold text-4xl tracking-wide uppercase montserrat gradient-text">
+              MonkFish Food Love Monkfish Foodies
+            </h1>
+            <p>Tasting and sharing all the different food</p>
+          </div>
+          <div className="text-3xl flex gap-0">
+            <Link className="hero-link custom-gradient" href="/community">
+              Join Us!
+            </Link>
+            <Link className="hero-link custom-gradient" href="/meals">
+              Browse meals...
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className="home-page-section">
+          <h2>How it works</h2>
+          <p>
+            Monkfish Food is a culinary haven where enthusiasts swap their top
+            meal secrets. It is a treasure trove for uncovering new flavors, and
+            a hub for mingling with fellow gastronomy aficionados.
+          </p>
+          <p>
+            Monkfish Food is a place to discover new dishes, and to connect with
+            other food lovers.
+          </p>
+        </section>
+
+        <section className="home-page-section">
+          <h2>Why Monkfish Food?</h2>
+          <p>
+            Monkfish Food is a culinary haven where enthusiasts swap their top
+            meal secrets. It is a treasure trove for uncovering new flavors, and
+            a hub for mingling with fellow gastronomy aficionados.
+          </p>
+          <p>
+            Monkfish Food is a place to discover new dishes, and to connect with
+            other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
