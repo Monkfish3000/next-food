806fbaab372d6bbb296d052428f43db32449a453
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-02-06 17:40:04 +0100

    not found error page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>
+        We were unable to locate the requested page. Please check url is
+        correct.
+      </p>
+    </main>
+  );
+};
+
+export default NotFound;
