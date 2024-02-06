100fa7ce2255cabef4b3de68e16cc4b4e9bfb0ac
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-02-06 17:58:18 +0100

    meal details page fetching meal

--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -1,9 +1,34 @@
+import { getMeal } from '../../../../lib/meals';
 import Image from 'next/image';
 
+import styles from './page.module.css';
+
 export default function MealDetails({ params }) {
+  const meal = getMeal(params.slug);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>{params.slug}</p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+          </p>
+          <p className={styles.summary}>Summary</p>
+        </div>
+      </header>
+      ;
+      <main className={styles.main}>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: '...',
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
