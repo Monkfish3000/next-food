35ead23ebce76e8d2f7870addc99c8bfaf879bc6
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-02-06 17:36:18 +0100

    error page for meals page

---
+++ src/app/meals/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>Something went wrong!</h1>
+      <p>Meals were not fetched successfully, please try again.</p>
+    </main>
+  );
+};
+
+export default Error;
