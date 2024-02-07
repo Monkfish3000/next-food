42b896255f6d131df208b82b3eb7cd076b4e6ee6
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-02-07 12:08:25 +0100

    added types to mealsgrid

--- src/components/meals/MealsGrid.tsx
+++ src/components/meals/MealsGrid.tsx
@@ -1,7 +1,24 @@
 import Meal from './Meal';
 import styles from './mealsgrid.module.css';
 
-const MealsGrid = ({ meals }) => {
+// title, summary, instructions, creator, creator_email, image, slug
+
+interface Meal {
+  id: string;
+  title: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+  image: string;
+  slug: string;
+}
+
+interface MealsGridProps {
+  meals: Meal[];
+}
+
+const MealsGrid = ({ meals }: MealsGridProps) => {
   return (
     <ul className={styles.meals}>
       {meals.map((meal) => (
